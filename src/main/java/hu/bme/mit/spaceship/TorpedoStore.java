85c9b3df47b944a3884ff9514aed2660fb8e11ea
Ádám Varga <44337519+vra6a@users.noreply.github.com>
2021-04-28 23:43:53 +0200

    removed comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -12,7 +12,7 @@ public class TorpedoStore {
   private Random generator = new Random();
 
   // rate of failing to fire torpedos [0.0, 1.0]
-  private double FAILURE_RATE = 0.0; //NOSONAR
+  private double FAILURE_RATE = 0.0;
 
   private int torpedoCount = 0;
 
